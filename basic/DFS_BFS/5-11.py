821383dead8833ea1d8ac1a7a63a952c7d881388
minjeongHEO <hmjoung9770@gmail.com>
2023-11-01 23:06:59 +0900

    수정

--- basic/DFS_BFS/5-11.py
+++ basic/DFS_BFS/5-11.py
@@ -8,27 +8,25 @@ graph = []
 for i in range(n):
     graph.append((list(map(int, input()))))
 
-
-print(graph)
+# print(graph)
 
 # 이동할 네 방향 정의 (상 하 좌 우)
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-def bfs(x,y):
+def bfs(x, y):
     # 덱라이브러리 써서 큐 구현
     queue = deque()
-
-    queue.append((x,y))
+    queue.append((x, y))
 
     #큐가 빈 상태일 때까지 반복한다
     while queue:
-        x,y = queue.popleft()
+        x, y = queue.popleft()
 
         #현재 위치에서 네 방향(상하좌우)으로의 위치 확인
         for i in range(4): # 0 1 2 3
             nx = x + dx[i]
-            ny = x + dx[i]
+            ny = y + dy[i]
 
             #이동했는데 미로 공간을 벗어나는 경우 예외처리
             if nx < 0 or nx >=n or ny <0 or ny >= m:
@@ -43,7 +41,8 @@ def bfs(x,y):
                 queue.append((nx,ny))
 
     #가장 오른쪽 아래까지의 최단거리 반환
-    return graph[n-1][m-1]
+    # return graph[n-1][m-1]
+    return graph[-1][-1]
 
 
 print(bfs(0,0))
