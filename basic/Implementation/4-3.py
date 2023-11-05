6eb030b61d842a25f22afb9c730c379cc35a25d3
minjeongHEO <hmjoung9770@gmail.com>
2023-11-05 17:26:11 +0900

    solve

--- basic/Implementation/4-3.py
+++ basic/Implementation/4-3.py
@@ -7,51 +7,40 @@
 # 행 위치는 숫자 1~8
 # 열 위치는 영어 a~h
 
-loc = input()
-x = int(loc[-1]) #행 (두번쨰자리)
+# 현재 나이트의 위치 입력받기
+input_data = input()
+row = int(input_data[1])
+column = int(ord(input_data[0])) - int(ord('a')) + 1
+'''
+input_data의 첫 번째 문자(열 위치)를 숫자로 변환하는 과정입니다. 
+ord() 함수는 문자의 ASCII(아스키) 코드 값을 반환합니다. 
+    (
+    print(ord('A'))  # 출력: 65
+    print(ord('a'))  # 출력: 97
+    print(ord('1'))  # 출력: 49
+    반대로 아스키 코드 값을 문자로 변환하고 싶을 때는 chr() 함수를 사용합니다. 
+    예를 들어, chr(65)는 'A'를 반환합니다.
+    )
+'a'의 아스키 코드 값은 97이므로, 'a'를 기준으로 열의 위치를 숫자로 변환합니다. 
 
-loc_y = loc[:1] #열 (첫번쨰자리)
-y_data = ['', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
-y = y_data.index(loc_y) #왜 find()는 에러나지?
+예를 들어, 'a'에서 'a'를 추출하면 아스키 코드 값이 동일하므로 0이 되고, 
+    여기에 1을 더하여 column에 1을 할당합니다.
+'''
 
-nx = [0, 0, 2, -4]
-ny = [2, -4, 0, 0]
-ny2 = [1, -2]
-count = 0
-print("----------------------")
-print(f"(x:{x}, y:{y})")
-print("----------------------")
-for i in range(4):
-    # 수직으로 두칸 이동 후 수평으로 한칸 0 1
-    # 수평으로 두칸 이동후 수직으로 한칸 2 3
-    x += nx[i] #0 / 1
-    y += ny[i] #0 / 1
+# 나이트가 이동할 수 있는 8가지 방향 정의
+# steps = [(-2, -1), (-1, -2), (1, -2), (2, -1), (2, 1), (1, 2), (-1, 2), (-2, 1)]
+steps = [(-2, -1), (-2, 1), (-1, -2), (1, -2), (2, -1), (2, 1), (1, 2), (-1, 2) ]
 
-    if i == 0 or i == 1:
-        # 수평으로 한칸
-        print("--------수평으로 두칸-------")
-        print(f"(x:{x}, y:{y})")
-        for j in range(2):
-            print("--------수직으로 한칸-------")
-            x += ny2[j]
-            print(f"(x:{x}, y:{y})")
-            if 0<x<9 and 0<y<9:
-                print(f"*****************count (x:{x}, y:{y})")
-                count += 1
+# 8가지 방향에 대하여 각 위치로 이동이 가능한지 확인
+result = 0
+for step in steps:
+    # print(type(step)) <class 'tuple'>
+    # 이동하고자 하는 위치 확인
+    next_row = row + step[0]
+    next_column = column + step[1]
+    # 해당 위치로 이동이 가능하다면 카운트 증가
+    if next_row >= 1 and next_row <= 8 and next_column >= 1 and next_column <= 8:
+        result += 1
 
-    else:
-        # 수직으로 한칸
-        print("--------수직으로 두칸-------")
-        print(f"(x:{x}, y:{y})")
-        for j in range(2):
-            print("--------수평으로 한칸-------")
-            y += ny2[j]
-            print(f"(x:{x}, y:{y})")
-            if 0<x<9 and 0<y<9:
-                print(f"*****************count (x:{x}, y:{y})")
-                count += 1
-
-print(count)
-
-
+print(result)
 
