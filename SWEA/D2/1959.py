36dfc6a523ad1171c260133eacc418f9582b4084
minjeongHEO <hmjoung9770@gmail.com>
2023-11-17 13:17:26 +0900

    다른풀이

--- SWEA/D2/1959.py
+++ SWEA/D2/1959.py
@@ -24,4 +24,41 @@ for tc in range(1, int(input())+1):
             result = max
 
     print(f'#{tc} {result}')
+'''
+58,796 kb
+메모리
+171 ms
+실행시간
+'''
+# ======================================================================
+# 다른 풀이
+# ======================================================================
+for tc in range(1, int(input())+1):
+    n, m = map(int, input().split())
+    a = list(map(int, input().split())) #n 7
+    b = list(map(int, input().split())) #m 6
 
+    multiple = []
+
+    if m>n:
+        for i in range(m-n+1):
+            sum = 0
+            for j in range(n):
+                sum += a[j] * b[j+i]
+
+            multiple.append(sum)
+    else:
+        for i in range(n-m + 1):
+            sum = 0
+            for j in range(m):
+                sum += a[j + i] * b[j]
+
+            multiple.append(sum)
+
+    print(f"#{tc} {max(multiple)}")
+'''
+58,264 kb
+메모리
+159 ms
+실행시간
+'''
